ded46429741fc8cba7159e25d481095ce96fb5d6
Patrickodida <patrick.o.odida@gmail.com>
2022-10-18 15:10:03 -0700

    adda two intergers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers.
+ * @num1: the first interger to be added
+ * @num2: the second interger to be added
+ *
+ * Return: the result of the addition
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
