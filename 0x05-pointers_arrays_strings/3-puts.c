5bb190dbf0d65dede1e85693aa038c4b40c5234b
Patrickodida <patrick.o.odida@gmail.com>
2022-10-24 03:41:43 -0700

    task 3 fixed

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,14 +3,13 @@
 /**
  * _puts - prints a string followed by a newline
  * @str: the string
- *
- * Return: void
  */
 
 void _puts(char *str)
 {
-	while (*str)
+	while (*str != '0\')
+	{
 		_putchar(*str++);
-
+	}
 	_putchar('\n');
 }
