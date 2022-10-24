5fe8dae9b9bb9e529a9c69ce918cf1505b630a28
Patrickodida <patrick.o.odida@gmail.com>
2022-10-24 02:05:37 -0700

    task3 update

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,10 +11,10 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i] != 0 '\0')
+	while ((str[i] != 0) '\0')
 	{
-		_putchar(str[i]);
+		_puts(str[i]);
 		i++;
 	}
-	_putchar('\n');
+	_puts('\n');
 }
