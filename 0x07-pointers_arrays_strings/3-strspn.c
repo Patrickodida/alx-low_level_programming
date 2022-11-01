91fa6ca92956b746572fd0196e859b941e097b55
Patrickodida <patrick.o.odida@gmail.com>
2022-10-31 20:03:30 -0700

    task3 fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,22 +10,20 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int bytes = 0;
-	int a;
+	int z = 0, x, y;
 
-	while (*s)
+	for (x = 0; s[x] != '\0'; x++)
 	{
-		for (index = 0; accept[a]; a++)
+		if (s[x] != 32)
 		{
-			if (*s == accept[a])
+			for (y = 0; accept[y] != '\0'; y++)
 			{
-				bytes++;
-				break
+				if (s[x] == accept[y])
+					z++;
 			}
-			else if (accept[a + 1] == '\0')
-				return (bytes);
 		}
-		s++;
+		else
+			return (z);
 	}
-	return (bytes);
+	return (z);
 }
