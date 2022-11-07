8e40dc0bd1cb9aee1d6fb1e004e988d5c35215df
Patrickodida <patrick.o.odida@gmail.com>
2022-11-07 06:17:30 -0800

    task1 fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,12 +17,13 @@ char *_strdup(char *str)
 		return (NULL);
 
 	for (i = 0; str[i] != '\0'; i++)
+		;
 		strout = (char *)malloc(sizeof(char) * (i + 1);
 
 				if (strout == NULL)
 				return (NULL);
 
 				for (j = 0; j <= i; j++)
-				strout[j] = str[j]
+				strout[j] = str[j];
 				return (strout);
 }
