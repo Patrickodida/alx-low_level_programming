4f583962fb64883b364f79c707ba0cd04d1d942a
Patrickodida <patrick.o.odida@gmail.com>
2022-11-07 03:26:17 -0800

    task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - a program that multiplies two numbers.
+ * @argc: argument count
+ * @argv: argument
+ *
+ * Return: 0 Success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
